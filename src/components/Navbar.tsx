88b801a937799a5c263b85cac1dc74464c8132f4
Ayush tomar <ayushtomar388@gmail.com>
2025-01-29 23:26:05 +0530

    navbar component created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return ( 
+        <div className="bg-neutral-900 py-2.5 px-4 flex justify-between">
+            <p className="text-neutral-200">Navbar</p>
+            <p className="bg-red-500/15 text-red-500 border border-red-500/25 rounded py-0.5 px-3 text-sm font-medium -2">Download Image</p>
+        </div>
+     );
+}
+ 
+export default Navbar;
