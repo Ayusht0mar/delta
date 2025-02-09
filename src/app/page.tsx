e37914e7af9b29e7c3f93823b08dfbd19f44c4dd
Ayush tomar <ayushtomar388@gmail.com>
2025-02-09 23:50:18 +0530

    packstats link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
     <div>
         <div className="text-white">
           <h2>Making changes into tools according to my needs.</h2>
-
           <ul>
             <li>
               <a href="/codeshots">CodeShots</a>
@@ -11,9 +10,28 @@ export default function Home() {
             <li>
               <a href="/framex">FrameX</a>
             </li>
+            <li>
+              <a href="/packstats">Packstats</a>
+            </li>
           </ul>
         </div>
 
     </div>
   );
 }
+
+import Image from "next/image";
+
+const Navbar = () => {
+    return ( 
+        <div className="bg-neutral-900 py-2.5 px-4 flex justify-between">
+            <div className="flex items-center gap-2">
+                <Image src="/logo.svg" alt="logo" width={24} height={24} />
+                {/* <p className="bg-neutral-500/15 text-neutral-500 border border-neutral-500/25 rounded py-0.5 px-3 text-sm font-medium -2">Select Tools</p> */}
+            </div>
+            {/* <p className="text-neutral-200">Navbar</p> */}
+            {/* <p className="bg-red-500/15 text-red-500 border border-red-500/25 rounded py-0.5 px-3 text-sm font-medium -2">Download Image</p> */}
+        </div>
+     );
+}
+ 
