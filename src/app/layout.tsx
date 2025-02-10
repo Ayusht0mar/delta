804a11ea84b89625d048116b45c680d125d7a1c1
Ayush tomar <ayushtomar388@gmail.com>
2025-02-10 23:45:54 +0530

    add: Toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-950 min-h-screen text-neutral-300`}
       >
+        <Toaster position="top-center" richColors  />
+
         <Navbar />
         {children}
       </body>
