336540c4a8d3fa2ef1e97f9f41b15af811fb9cde
Ayush tomar <ayushtomar388@gmail.com>
2025-02-11 23:52:54 +0530

    add: Themes style

---
+++ src/types/CodeshotsThemes.ts
@@ -0,0 +1,12 @@
+export const themes = {
+  hyper: "bg-gradient-to-br from-fuchsia-500 via-red-600 to-orange-400",
+  oceanic: "bg-gradient-to-br from-green-300 via-blue-500 to-purple-600",
+  candy: "bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400",
+  sublime: "bg-gradient-to-br from-rose-400 via-fuchsia-500 to-indigo-500",
+  horizon: "bg-gradient-to-br from-orange-500 to-yellow-300",
+  coral: "bg-gradient-to-br from-blue-400 to-emerald-400",
+  peach: "bg-gradient-to-br from-rose-400 to-orange-300",
+  flamingo: "bg-gradient-to-br from-pink-400 to-pink-600",
+  gotham: "bg-gradient-to-br from-gray-700 via-gray-900 to-black",
+  ice: "bg-gradient-to-br from-rose-100 to-teal-100",
+};
