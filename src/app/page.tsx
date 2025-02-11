25bd486cdd9c8c9b7ba7109a6f144465de837673
Ayush tomar <ayushtomar388@gmail.com>
2025-02-11 23:54:41 +0530

    remove: Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 export default function Home() {
   return (
     <div>
@@ -14,24 +15,13 @@ export default function Home() {
               <a href="/packstats">Packstats</a>
             </li>
           </ul>
+
+
+
         </div>
 
     </div>
   );
 }
 
-import Image from "next/image";
 
-const Navbar = () => {
-    return ( 
-        <div className="bg-neutral-900 py-2.5 px-4 flex justify-between">
-            <div className="flex items-center gap-2">
-                <Image src="/logo.svg" alt="logo" width={24} height={24} />
-                {/* <p className="bg-neutral-500/15 text-neutral-500 border border-neutral-500/25 rounded py-0.5 px-3 text-sm font-medium -2">Select Tools</p> */}
-            </div>
-            {/* <p className="text-neutral-200">Navbar</p> */}
-            {/* <p className="bg-red-500/15 text-red-500 border border-red-500/25 rounded py-0.5 px-3 text-sm font-medium -2">Download Image</p> */}
-        </div>
-     );
-}
- 
