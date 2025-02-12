07101f030ada612c3c432ab5d7b31a4ba6e174d2
Ayush tomar <ayushtomar388@gmail.com>
2025-02-12 22:46:19 +0530

    add: navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,34 @@
+"use client"
+import Image from "next/image";
+import PageNav from "./PageNav";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+
+const options = [
+    { value: "/codeshots", label: "CodeShots" },
+    { value: "/framex", label: "FrameX" },
+    { value: "/packstats", label: "Packstats" },
+
+];
+
+
 const Navbar = () => {
+
+    const router = usePathname();
+
     return ( 
-        <div className="bg-neutral-900 py-2.5 px-4 flex justify-between">
-            <p className="text-neutral-200">Navbar</p>
-            <p className="bg-red-500/15 text-red-500 border border-red-500/25 rounded py-0.5 px-3 text-sm font-medium -2">Download Image</p>
+        <div className="bg-neutral-900 absolute w-full py-1.5 px-4 flex items-center justify-between top-0 z-50 shadow shadow-white/10" suppressHydrationWarning>
+            <div className="flex items-center gap-2">
+                <Link href="/">
+                    <Image src="/logo.svg" alt="logo" width={24} height={24} />
+                </Link>
+
+                <PageNav options={options} currentPath={router} />
+            </div>
+            <p className="text-neutral-500">
+                Built by <a href="https://dub.sh/ayushtomar" target="_" className="text-neutral-300 hover:text-neutral-200 px-1">Ayush Tomar</a>
+            </p>
         </div>
      );
 }
