dae75c29b23a15675c7082a43d352379cb943890
Ayush tomar <ayushtomar388@gmail.com>
2025-02-12 22:35:35 +0530

    Update : Style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,22 @@
 export default function Home() {
   return (
     <div>
-        <div className="text-white">
-          <h2>Making changes into tools according to my needs.</h2>
-          <ul>
-            <li>
-              <a href="/codeshots">CodeShots</a>
-            </li>
-            <li>
-              <a href="/framex">FrameX</a>
-            </li>
-            <li>
-              <a href="/packstats">Packstats</a>
-            </li>
-          </ul>
-
-
-
+        <div className="text-white flex flex-col items-center justify-center h-screen">
+          <div className="space-y-3 flex flex-col">
+            <a href="/codeshots" className="bg-neutral-800 border-neutral-600 border p-4 rounded">
+              <p>CodeShots</p>
+              <p className="text-neutral-400">A tool to make code snippets image.</p>
+            </a>
+            <a href="/framex" className="bg-neutral-800 border-neutral-600 border p-4 rounded">
+              <p>FrameX</p>
+              <p className="text-neutral-400">A tool to make Twitter/X post image.</p>
+            </a>
+            <a href="/packstats" className="bg-neutral-800 border-neutral-600 border p-4 rounded">
+              <p>Packstats</p>
+              <p className="text-neutral-400">A tool to see stats of your npm packs.</p>
+            </a>
+          </div>
         </div>
-
     </div>
   );
 }
